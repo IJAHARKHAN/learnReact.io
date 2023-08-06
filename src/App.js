ca39cbafd339aff62eba11ea8cbb90fb090ff330
IJAHARKHAN <khan7oct@gmail.com>
2023-08-06 18:50:01 +0530

    CommentOut about.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function AppMoonFunction() {
       {/* <Navbar title="TextUtils" aboutText="About" homeText="Home"/> */}
       <Navbar title="TextUtilsApp"/>
       <TextForm  title="Utilities"/>
-      {/* <About/> */}
+      <About/>
     </>
   );
 }
